897f300c3e5a7f41841ad8ab09aaf171d1352f70
Thilak Raj TK <43881661+GST216@users.noreply.github.com>
2026-09-05 15:30:19 +0530

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
+import { Spinner } from "./core/spinner.js";
+import { DomRenderer } from "./renderer/dom/dom-renderer.js";
+import type {
+  MountableSpinner,
+  SpinnerController,
+  SpinnerOptions,
+  SpinnerVariant,
+} from "./core/types.js";
 
+export type {
+  MountableSpinner,
+  SpinnerController,
+  SpinnerOptions,
+  SpinnerVariant,
+};
+
+export function createSpinner(
+  options: SpinnerOptions = {},
+): Spinner {
+  return new Spinner(new DomRenderer(), options);
+}
